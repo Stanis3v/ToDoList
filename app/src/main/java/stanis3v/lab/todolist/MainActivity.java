7068d9050ecf3a81a6f32a3681f9b336c490197c
Stanis <stanisvel.dev@gmail.com>
2021-07-15 21:32:16 +0200

    main

--- app/src/main/java/stanis3v/lab/todolist/MainActivity.java
+++ app/src/main/java/stanis3v/lab/todolist/MainActivity.java
@@ -1,4 +1,27 @@
 package stanis3v.lab.todolist;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darktheme);
+        } else {
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById((R.id.listView));
+    }
+
 }
