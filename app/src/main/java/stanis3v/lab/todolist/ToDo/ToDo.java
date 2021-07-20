cdbab00d83d1b252b7abd896f52c723602e33b57
Stanis <stanisvel.dev@gmail.com>
2021-07-20 10:14:09 +0200

    Code

--- app/src/main/java/stanis3v/lab/todolist/ToDo/ToDo.java
+++ app/src/main/java/stanis3v/lab/todolist/ToDo/ToDo.java
@@ -1,4 +1,47 @@
 package stanis3v.lab.todolist.ToDo;
 
 public class ToDo {
+    private int id;
+    private String title;
+    private String des;
+    private String remind;
+
+    public ToDo(int id, String title, String des, String remind) {
+        this.id = id;
+        this.title = title;
+        this.des = des;
+        this.remind = remind;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getRemind() {
+        return remind;
+    }
+
+    public void setRemind(String remind) {
+        this.remind = remind;
+    }
 }
